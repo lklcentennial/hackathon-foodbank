4a2c7be28444208b3b3566c2b7cf5a4d93d3987c
francisallin <lamtszto1997@gmail.com>
2023-11-07 14:40:38 -0500

    create inventory jsx and dummy data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 import home from "../app/icons/home.png"
 import location from '../app/icons/location.png';
 import login from '../app/icons/login.png';
@@ -7,10 +7,10 @@ import login from '../app/icons/login.png';
 
 const Navbar = () => {
   return (
-    <div className="fixed inset-x-0 bottom-0 bg-blue-500 flex justify-around p-4">
-      <button className="text-white"><img src={home.src} alt="home" width={30}/>Home</button>
-      <button className="text-white"><img src={location.src} alt="location" width={30}/>Map</button>
-      <button className="text-white"><img src={login.src} alt="login" width={30}/>Log In</button>
+    <div className="fixed text-black text-center inset-x-0 bottom-0 bg-blue-500 flex justify-around p-4 invert">
+      <Link href='#' className=""><img src={home.src} alt="home" width={30}/>Home</Link>
+      <Link href='#' className="text-black "><img src={location.src} alt="location" width={30}/>Map</Link>
+      <Link href='#' className="text-black "><img src={login.src} alt="login" width={30}/>Log In</Link>
     </div>
   );
 };
