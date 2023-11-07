d504bcaf92f41bb5e99f0352c6b29cbf1d2744e9
francisallin <lamtszto1997@gmail.com>
2023-11-07 15:53:53 -0500

    update navbar colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,17 @@ import login from "../app/icons/login.png";
 const Navbar = () => {
   const test = "Home";
   return (
-    <div className="fixed text-black text-center inset-x-0 bottom-0 bg-blue-500 flex justify-around p-4 invert">
-      <Link href="./Inventory" className="flex flex-col items-center justify-center">
-        <img src={home.src} alt="home" width={30} />
+    <div className="fixed text-white text-center inset-x-0 bottom-0 bg-slate-700 flex justify-around p-4 ">
+      <Link href="#" className="flex flex-col items-center justify-center">
+        <img src={home.src} alt="home" width={30} className="invert"/>
         <p>{test}</p>
       </Link>
       <Link href="#" className="flex flex-col items-center justify-center">
-        <img src={location.src} alt="location" width={30} />
+        <img src={location.src} alt="location" width={30} className="invert"/>
         <p>{test}</p>
       </Link>
       <Link href="#" className="flex flex-col items-center justify-center">
-        <img src={login.src} alt="login" width={30} />
+        <img src={login.src} alt="login" width={30} className="invert"/>
         <p>{test}</p>
       </Link>
     </div>
