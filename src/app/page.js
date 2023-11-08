d95cffebb9288a55ec609470e397fe756715d8d4
francisallin <lamtszto1997@gmail.com>
2023-11-07 22:03:48 -0500

    add search and highlight item in inventory

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,14 @@ import CenterContainer from "@/components/CenterContainer";
 import SignInButton from "@/components/SignIn";
 import SignOutButton from "@/components/SignOutButton";
 import { useSession } from "next-auth/react";
-import Navbar from "@/components/Navbar";
-import Inventory from "@/components/Inventory"
 import { useRouter } from "next/navigation";
+import Inventory from "@/components/Inventory";
 
 export default function Home() {
   const session = useSession();
   const router = useRouter();
   return (
     <CenterContainer>
-      <Navbar/>
       {session.data ? (
         <>
           <h1 className="m-2 text-4xl font-bold text-gray-900">
--- src/components/FBManagerView.jsx
+++ src/components/FBManagerView.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Inventory from "./Inventory";
 
 const FlexContainer = ({ children }) => {
   return (
     <div className={`m-4 flex flex-col`}>
+      <Navbar/>
+      <Inventory/>
       {children}
     </div>
   );
